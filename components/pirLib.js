43948eb78d2eb778e5e8ff9bdefc34235c3233cd
lzptr <26966675+lzptr@users.noreply.github.com>
2025-02-23 13:19:00 +0100

    Fixed pirLib

--- components/pirLib.js
+++ components/pirLib.js
@@ -11,23 +11,24 @@ class PIR {
     this.callback = callback
     this.default = {
       debug: this.config.debug,
-      gpio: 21,
+      gpio: 14,  // Changed default to 14
       reverseValue: false
     }
     this.config = Object.assign({}, this.default, this.config)
     if (this.config.debug) log = (...args) => { console.log("[MMM-Pir] [LIB] [PIR]", ...args) }
     this.running = false
     this.PathScript = path.dirname(require.resolve('../package.json')) + "/scripts"
+    log("Initialized with GPIO:", this.config.gpio)  // Debug log
     this.callback("PIR_INITIALIZED")
   }
 
   start() {
     if (this.running) return
-    log("Start")
+    log("Start monitoring on GPIO", this.config.gpio)  // Debug log
     try {
       this._startStateMonitoring()
       this.callback("PIR_STARTED")
-      console.log("[MMM-Pir] [LIB] [PIR] Started!")
+      console.log("[MMM-Pir] [LIB] [PIR] Started on GPIO", this.config.gpio)
     } catch (err) {
       console.error("[MMM-Pir] [LIB] [PIR] " + err)
       this.running = false
@@ -37,28 +38,44 @@ class PIR {
 
   _startStateMonitoring() {
     this.running = true
-    // Monitor GPIO state using Python script
-    this.stateInterval = setInterval(() => {
-      exec(`python monitor.py -s -g=${this.config.gpio}`, { cwd: this.PathScript }, (err, stdout, stderr) => {
-        if (err) {
-          console.error("[MMM-Pir] [LIB] [PIR] " + err)
-          return this.callback("PIR_ERROR", err.message)
-        }
-        const value = parseInt(stdout.trim())
-        log("Sensor read value: " + value)
-        if ((value == 1 && !this.config.reverseValue) || (value == 0 && this.config.reverseValue)) {
+    // Monitor GPIO state using Python script with debug
+    exec(`python monitor.py -v -s -g=${this.config.gpio}`, { cwd: this.PathScript }, (err, stdout, stderr) => {
+      if (err) {
+        console.error("[MMM-Pir] [LIB] [PIR] Initial state check failed:", err)
+        return
+      }
+      log("Initial state check:", stdout.trim())
+    })
+
+    // Start continuous monitoring
+    this.monitorProcess = exec(`python monitor.py -v -m -g=${this.config.gpio}`, { cwd: this.PathScript })
+
+    this.monitorProcess.stdout.on('data', (data) => {
+      const lines = data.toString().trim().split('\n')
+      lines.forEach(line => {
+        if (line === '1') {
+          log("Motion detected on GPIO", this.config.gpio)
           this.callback("PIR_DETECTED")
-          log("Detected presence (value: " + value + ")")
         }
       })
-    }, 1000) // Check every second
+    })
+
+    this.monitorProcess.stderr.on('data', (data) => {
+      console.error("[MMM-Pir] [LIB] [PIR] Error:", data.toString())
+    })
+
+    this.monitorProcess.on('close', (code) => {
+      if (code !== 0) {
+        console.error("[MMM-Pir] [LIB] [PIR] Monitor process exited with code", code)
+      }
+    })
   }
 
   stop() {
     if (!this.running) return
-    if (this.stateInterval) {
-      clearInterval(this.stateInterval)
-      this.stateInterval = null
+    if (this.monitorProcess) {
+      this.monitorProcess.kill()
+      this.monitorProcess = null
     }
     this.running = false
     this.callback("PIR_STOP")
